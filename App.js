df58720df74a93ede1384b37849349cc0abf2c05
Srivani <gogulasrivani6@gmail.com>
2025-06-17 18:11:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ const app=express();
 app.set('trust proxy', true);
 const PORT = 3000;
 app.get('/api/whoami',(req,res)=>{
-    const ipvalue = req.ip;
+    const ipaddress = req.ip;
     const language = req.headers['accept-language'];
     const software = req.headers['user-agent'];
-    res.json({ipaddress:ipvalue,
-        language : language,
-        software : software
+    res.json({ipaddress,
+        language,
+        software
     });
 });
 
