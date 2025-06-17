d5261356b220b77ea169de96d2eca56dae617c0b
Srivani <gogulasrivani6@gmail.com>
2025-06-17 17:24:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app=express();
+
+const PORT = 3000;
+app.get('/api/whoami',(req,res)=>{
+    const ipvalue = req.ip;
+    const language = req.headers['accept-language'];
+    const software = req.headers['user-agent'];
+    res.json({ipaddress:ipvalue,
+        language : language,
+        software : software
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log("welcome to request parser");
+});
+
+module.exports = app;
