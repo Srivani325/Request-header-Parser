d86aa1f2781ed89dcf6c97e1734a49e9a807cf81
Srivani <gogulasrivani6@gmail.com>
2025-06-18 11:52:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ app.get('/api/whoami',(req,res)=>{
     const ipaddress =  req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
     // const language = req.headers['accept-language'];
     // const software = req.headers['user-agent'];
-    res.json({ipaddress,
+    res.json({ipaddress
         // language,
         // software
     });
