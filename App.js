19f6da81ad664084cd5fe10ed0bbfb65fa2b2285
Srivani <gogulasrivani6@gmail.com>
2025-06-19 18:34:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ app.get('/api/whoami',(req,res)=>{
     const ipaddress =  req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const language = req.headers['accept-language'];
     const software = req.headers['user-agent'];
-    res.json({ipaddress: ip,
+    res.json({ipaddress: ipaddress,
     language: language,
     software: software
     });
